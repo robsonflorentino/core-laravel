7d3a962014a7693b1d6d382072dc469d386489a3
Robson Florentino <robson.florentino.silva@gmail.com>
2015-08-03 18:09:26 -0300

    Ajustando templates blade

---
+++ resources/views/layouts/sidebar-group/sidebar.blade.php
@@ -0,0 +1,19 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar user panel (optional) -->
+    @include('layouts/sidebar-group/user_panel')
+
+    <!-- search form (Optional) -->
+    @include('layouts/sidebar-group/search_form')
+
+    <!-- Sidebar Menu -->
+    @include('layouts/sidebar-group/sidebar_menu')
+
+  </section>
+  <!-- /.sidebar -->
+  
+</aside>
